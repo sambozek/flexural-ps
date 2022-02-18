9910ab2ae28993b00d87472ae01091a6f1a4fa59
Sam Bozek <sambozek@gmail.com>
2022-02-18 13:14:27 -0800

    Prep for heroku deploy

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = Dash(__name__)
 
-df = pd.read_csv("./all_flex_results.csv")
+df = pd.read_csv("https://gist.githubusercontent.com/sambozek/8b3af6bbc873402ae7b5678d30c5c8b2/raw/963c30fd0d87c09da1f71cf352fdeae6a3e8afdd/flex_0218.csv")
 df['Date'] = pd.to_datetime(df['Date'], format='%m_%d_%Y' )
 df['Formulation'] = df['Experiment'].apply(lambda x: x.split('_')[0])
 df['Formulation'] = df['Formulation'].apply(lambda x: x.split('-')[0])
@@ -14,7 +14,7 @@ fig = px.box(df, x='Formulation', y='flex modulus')
 
 app.layout = html.Div(children=[
     html.H1(
-        children='Flexural Modulus Distribution By Date',
+        children='Flexural Modulus Distribution By Formulation',
         style={
             'textAlign':'Center'
         }
