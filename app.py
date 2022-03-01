44b88fb5fbea883de21d38bc5e12f9e3ef9a85ca
Samuel Bozek <sambozek@gmail.com>
2022-02-28 19:37:51 -0800

    Ordered by median of the flexural modulus

--- app.py
+++ app.py
@@ -7,16 +7,24 @@ app = Dash(__name__)
 
 server = app.server
 
-df = pd.read_csv("https://gist.githubusercontent.com/sambozek/8b3af6bbc873402ae7b5678d30c5c8b2/raw/963c30fd0d87c09da1f71cf352fdeae6a3e8afdd/flex_0218.csv")
+df = pd.read_csv("https://gist.githubusercontent.com/sambozek/8b3af6bbc873402ae7b5678d30c5c8b2/raw/b5445235ab7139b36283d4ee987a3969b6014425/flex_0218.csv")
 df['Date'] = pd.to_datetime(df['Date'], format='%m_%d_%Y' )
 df['Formulation'] = df['Experiment'].apply(lambda x: x.split('_')[0])
 df['Formulation'] = df['Formulation'].apply(lambda x: x.split('-')[0])
+
+df_grouped = df.groupby(["Formulation"])
+df_meds = pd.DataFrame({col:vals['flex modulus'] for col, vals in df_grouped})
+
+meds = df_meds.median()
+meds.sort_values(ascending=False, inplace=True)
+
+df_meds = df_meds[meds.index]
 df2 = df.sort_values(by=['Formulation'], ascending=False)
-fig = px.box(df2, x='Formulation', y='flex modulus', points='all')
+fig = px.box(df2, x='Formulation', y='flex modulus', points='all', category_orders={'Formulation': meds.index})
 
 app.layout = html.Div(children=[
     html.H1(
-        children='Flexural Modulus Distribution By Formulation',
+        children='Flexural Modulus Distribution By Median',
         style={
             'textAlign':'Center'
         }
